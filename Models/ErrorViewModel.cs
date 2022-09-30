8fcb70af4ada6bc5af38936e91a871be9b5fd9ee
Chancesus <chancekgsmith@gmail.com>
2022-09-30 15:07:19 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eCommerceMovieTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
